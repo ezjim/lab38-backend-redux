3bffb8c4fe7f78d7d35e44eb880f0fcb2f149aa4
james eserjose <james.re@yahoo.com>
2020-05-13 15:26:47 -0700

    routes / test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,59 @@
 const mongoose = require('mongoose');
 const request = require('supertest');
 const app = require('../lib/app');
-
-describe('lab38-be routes', () => {
+const Todo = require('../lib/models/Todo');
+const connect = require('../lib/utils/connect');
+  
+describe('lab38-be Routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+    
   beforeEach(() => {
     return mongoose.connection.dropDatabase();
+  });
+    
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+  
+  it('creates a Todo via post', () => {
+    return request(app)
+      .post('/api/v1/todos')
+      .send({
+        title: 'finish your labs',
+        description: 'lab29/34',
+        target: 'are you close?'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          title: 'finish your labs',
+          description: 'lab29/34',
+          target: 'are you close?',
+          completed: false,
+          __v: 0
+        });
+      });
+  });
+  it('gets a list of Todos via GET', async() => {
+    await Todo.create({
+      title: 'finish your labs',
+      description: 'lab29/34',
+      target: 'are you close?'
+    });
+    
+    return request(app)
+      .get('/api/v1/todos')
+      .then(res => {
+        expect(res.body).toEqual([{
+          _id: expect.any(String),
+          title: 'finish your labs',
+          description: 'lab29/34',
+          target: 'are you close?',
+          completed: false,
+          __v: 0
+        }]);
+      });
   });
 });
